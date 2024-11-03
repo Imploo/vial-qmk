fce96d10ea393bf0e55683d5a5dc7b4a5ec2d19f
Imploo <m.bultje@outlook.com>
2024-11-03 13:00:50 +0100

    Small changes to layout

--- keyboards/imploo/bignomad/rp2040_ce/config.h
+++ keyboards/imploo/bignomad/rp2040_ce/config.h
@@ -1,5 +1,3 @@
-// Copyright 2024 Markus Knutsson (@TweetyDaBird)
-// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
 #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
@@ -7,3 +5,4 @@
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP2
 #define I2C1_SCL_PIN GP3
+#define MASTER_LEFT
